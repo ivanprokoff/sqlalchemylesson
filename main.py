928a2049c164fce09ecf447e2b5adb2e4a053268
ivanprokoff <ivanprokoff@yandex.ru>
2021-03-11 18:07:59 +0500

    3 users added

--- main.py
+++ main.py
@@ -3,14 +3,24 @@ from data.users import User
 
 db_session.global_init("db/colonisation.db")
 
-user = User()
-user.name = "Ridley"
-user.surname = 'Scott'
-user.age = 21
-user.position = 'captain'
-user.speciality = 'research engineer'
-user.address = 'module_1'
-user.email = "scott_chief@mars.org"
-db_sess = db_session.create_session()
-db_sess.add(user)
-db_sess.commit()
+dct = {'names': ['Ridley', 'John', 'John1', 'John2'],
+       'surnames': ['Scott', 'Brown', 'Brown1', 'Brown2'],
+       'ages': [21, 24, 27, 30],
+       'positions': ['captain', 'crew member', 'crew member', 'crew member'],
+       'specialities': ['research engineers', 'engineer', 'just funny guy', 'hmph'],
+       'adresses': ['module_1', 'module 2', 'module 3', 'module 3'],
+       'emails': ['scott_chief@mars.org', '1@mars.org', '2@mars.org', '3@mars.org']
+}
+
+for i in range(4):
+    user = User()
+    user.name = dct['names'][i]
+    user.surname = dct['surnames'][i]
+    user.age = dct['ages'][i]
+    user.position = dct['positions'][i]
+    user.speciality = dct['specialities'][i]
+    user.address = dct['adresses'][i]
+    user.email = dct['emails'][i]
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
